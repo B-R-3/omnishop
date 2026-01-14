0a008f6fea39443d7ec433277bfd67b090956aec
B-R-3 <brandon.molinaserna@limayrac.fr>
2026-01-14 20:30:18 +0100

    first commit

---
+++ src/main/java/com/br/omnishop/OmnishopApplication.java
@@ -0,0 +1,13 @@
+package com.br.omnishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/omnishop/OmnishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.omnishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
